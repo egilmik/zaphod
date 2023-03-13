784059ab60bcf7c80b77fb02180cf50223563539
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2023-03-13 09:26:15 +0100

    Setup board from FEN, excluding extra info(castling etc). Plus printing to console of board

--- board.cpp
+++ board.cpp
@@ -1,5 +1,6 @@
 #include "board.h"
 #include <iostream>
+#include <string>
 
 Board::Board(){
 
@@ -9,6 +10,10 @@ void Board::parseFen(std::string fen){
     int count = 0;
     int state = 0;
     for(std::string::size_type i = 0; i < fen.size(); ++i) {
+
+        if(fen[i] == ' '){
+            state++;
+        }
 
         switch (state)
         {
@@ -27,12 +32,84 @@ void Board::parseFen(std::string fen){
 void Board::parseFenPosition(char value, int &count){
 
     int bitNr = fenToBitMapping[count];
+
+    if(isdigit(value)){
+        std::string s = &value;
+        int increment = std::stoi(s);
+        count+= increment;
+    } else { 
+
+        switch (value)
+        {
+        case 'r':
+            setBit(pieceses, true, bitNr);
+            setBit(blackRooks,true, bitNr);
+            count++;
+            break;
+        case 'R':
+            setBit(pieceses, true, bitNr);
+            setBit(whiteRooks,true, bitNr);
+            count++;
+            break;
+        case '/':
+            //Do nothing
+            break;
+        case 'n':
+            setBit(pieceses,true, bitNr);
+            setBit(blackKnights,true,bitNr);
+            count++;
+            break;
+        case 'N':
+            setBit(pieceses,true, bitNr);
+            setBit(whiteKnights,true,bitNr);
+            count++;
+            break;
+        case 'b':
+            setBit(pieceses,true, bitNr);
+            setBit(blackBishops,true,bitNr);
+            count++;
+            break;
+        case 'B':
+            setBit(pieceses,true, bitNr);
+            setBit(whiteBishops,true,bitNr);
+            count++;
+            break;
+        case 'q':
+            setBit(pieceses,true, bitNr);
+            setBit(blackQueens,true,bitNr);
+            count++;
+            break;
+        case 'Q':
+            setBit(pieceses,true, bitNr);
+            setBit(whiteQueens,true,bitNr);
+            count++;
+            break;
+        case 'k':
+            setBit(pieceses,true, bitNr);
+            setBit(blackKing,true,bitNr);
+            count++;
+            break;
+        case 'K':
+            setBit(pieceses,true, bitNr);
+            setBit(whiteKing,true,bitNr);
+            count++;
+            break;
+        case 'p':
+            setBit(pieceses,true, bitNr);
+            setBit(blackPawns,true,bitNr);
+            count++;
+            break;
+        case 'P':
+            setBit(pieceses,true, bitNr);
+            setBit(whitePawns,true,bitNr);
+            count++;
+            break;
+
     
-    if(fenToBitboardMap.count(value) > 0){
-        BitBoard board = *fenToBitboardMap.find(value)->second;
-        setBit(pieceses, true, bitNr);
-        setBit(board,true, bitNr);
-        count++;
+        default:
+            count++;
+            break;
+        }
     }
     
 }
@@ -61,18 +138,33 @@ void Board::printBoard(){
             printBoard[i] = 'N';
         } else if(checkBit(blackKnights,i)){
             printBoard[i] = 'n';
+        } else if(checkBit(whiteBishops,i)){
+            printBoard[i] = 'B';
+        } else if(checkBit(blackBishops,i)){
+            printBoard[i] = 'b';
+        } else if(checkBit(whiteQueens,i)){
+            printBoard[i] = 'Q';
+        } else if(checkBit(blackQueens,i)){
+            printBoard[i] = 'q';
+        } else if(checkBit(whiteKing,i)){
+            printBoard[i] = 'K';
+        } else if(checkBit(blackKing,i)){
+            printBoard[i] = 'k';
+        } else if(checkBit(whitePawns,i)){
+            printBoard[i] = 'P';
+        } else if(checkBit(blackPawns,i)){
+            printBoard[i] = 'p';
         }
     }
 
-    for(int i = 64;i > 0; i--){
+    for(int i = 0;i < 64; i++){
         if(i%8== 0){
             std::cout << std::endl;
         }
-        std::cout << printBoard[i-1] << " ";
+        std::cout << printBoard[i] << " ";
     }
 
     std::cout << std::endl;
-
 
     
 }
--- board.h
+++ board.h
@@ -43,7 +43,7 @@ class Board {
                                     8, 9,10,11,12,13,14,15,
                                     0, 1, 2, 3, 4, 5, 6, 7 };
 
-        std::map<char,BitBoard*> fenToBitboardMap = {{'r', *blackRooks},
+        std::map<char,BitBoard*> fenToBitboardMap = {{'r', &blackRooks},
                                                     {'R', &whiteRooks},
                                                     {'n', &blackKnights},
                                                     {'N', &whiteKnights},
