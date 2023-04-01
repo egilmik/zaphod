5a8ab714a7d659aaa3cb272490fce0db1d082384
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2023-04-01 18:51:24 +0200

    Tests are up and running

--- test/testboard.cpp
+++ test/testboard.cpp
@@ -2,9 +2,16 @@
 #include "../src/board.h"
 
 // Demonstrate some basic assertions.
-TEST(BoardTest, BasicNotationTest) {
+TEST(BoardTest, a1NotationTest) {
   Board board;
   std::string actualNotation = board.sqToNotation[0];
 
   EXPECT_EQ(actualNotation, "a1");
+}
+
+TEST(BoardTest, a8NotationTest) {
+  Board board;
+  std::string actualNotation = board.sqToNotation[56];
+
+  EXPECT_EQ(actualNotation, "a8");
 }
