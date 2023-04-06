27d751333e58293fd0221f53ef42b707f1d1cf50
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2023-04-06 14:48:27 +0200

    Cleaned up output for tets

--- test/testperft.cpp
+++ test/testperft.cpp
@@ -29,9 +29,13 @@ TEST(PerftTest, perftStartingPosition){
     PerftResults results;
     Perft::perftWithStats(board,6,results);
 
-    EXPECT_EQ(20+400+8902+197281+4865609+119060324 ,results.nodes);
-    EXPECT_EQ(34+1576+82719+2812008, results.captures);
-    EXPECT_EQ(258+5248, results.enPassant);
+    int actualNodes =20+400+8902+197281+4865609+119060324;
+    int actualCapture = 34+1576+82719+2812008;
+    int actualEnPassant = 258+5248;
+
+    EXPECT_EQ(actualNodes ,results.nodes);
+    EXPECT_EQ(actualCapture, results.captures);
+    EXPECT_EQ(actualEnPassant, results.enPassant);
     EXPECT_EQ(0,results.castle);
     EXPECT_EQ(0, results.promotions);
 }
