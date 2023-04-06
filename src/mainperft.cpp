dcc2e5a9a132d7063df61cc189d427ab61f140b0
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2023-04-06 14:26:42 +0200

    Added perft executable for running perft

---
+++ src/mainperft.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "board.h"
+#include "movegenerator.h"
+#include "perft.h"
+#include "test.h"
+#include <chrono>
+
+int main(int, char**) {
+
+    Test test;
+    test.runAllTest();    
+        
+}
