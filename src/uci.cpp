cb063bb5817bc19a3b225be126a740ed2d02b6bb
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2024-06-01 17:23:22 +0200

    Changed version to 1.1-beta in UCI

--- src/uci.cpp
+++ src/uci.cpp
@@ -176,7 +176,7 @@ void UCI::startSearch(std::istringstream &is)
 
 void UCI::sendID()
 {
-    std::cout << "id name Zaphod 1.0.1" << std::endl;
+    std::cout << "id name Zaphod 1.1-beta" << std::endl;
     std::cout << "id author Egil Tennfjord Mikalsen" << std::endl;
     std::cout << "uciok" << std::endl;
 }
