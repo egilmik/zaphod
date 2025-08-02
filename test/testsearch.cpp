3fc03ba2e599f47ae8d49f3fed145e4a9b343c37
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2025-08-02 07:20:48 +0200

    Added first version of see, still missing snipers and pins. Not fast yet either, but performs similar to previous version on benchmark

--- test/testsearch.cpp
+++ test/testsearch.cpp
@@ -30,4 +30,26 @@ TEST_F(SearchTest, startingPosShouldReturnZeroScore){
     EXPECT_TRUE(score < 100);
 }
 
+TEST_F(SearchTest, seePositiveExchangeForBlack1) {
+    Board board;
+    board.parseFen("5rk1/1pp2q1p/p1pb4/8/3P1NP1/2P5/1P1BQ1P1/5RK1 b - -");
+    Move move = Move::make<NORMAL>(43, 29);
+
+    Search search;
+    int score = search.see(board,43,29,Black);
+    std::cout << score << std::endl;
+    EXPECT_TRUE(score > 0);
+}
+
+TEST_F(SearchTest, seePositiveExchangeForBlack2) {
+    Board board;
+    board.parseFen("3qk3/3r4/8/8/8/3R4/8/3QK3 b HAha - 0 1");
+    Move move = Move::make<NORMAL>(51, 19);
+
+    Search search;
+    int score = search.see(board, 51, 19, Black);
+    std::cout << score << std::endl;
+    EXPECT_TRUE(score > 0);
+}
+
 
