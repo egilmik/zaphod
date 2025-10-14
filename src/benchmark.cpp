c75bf40d16f5ff9b8a2eb5c725fc404a4772d20d
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2025-10-14 17:27:08 +0200

    Added aspiration window statistics

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -102,6 +102,7 @@ struct BenchmarkDefinition {
         std::cout << "Best move " << Perft::getNotation(move.bestMove)  << " Expected best move " << def.bestMove << std::endl;
         std::cout << "TT stats " << " Upper bound hit: " << search.upperBoundHit << " Lower bound hit " << search.lowerBoundHit << " Exact hit " << search.exactHit << " Pawn tt hits: " << search.pawnTTHits << std::endl;
         std::cout << "LMR stats " << " LMR hit: " << search.lmrHit << " LMR re-search: " << search.lmrResearchHit << std::endl;
+        std::cout << "Aspiration window stats " << " Low research : " << search.aspirationLowResearchHit << " High research: " << search.aspirationHighResearchHit << std::endl;
         /*std::cout << "PV ";
         for (int i = 0; i < pvList.counter; i++) {
             std::cout << Perft::getNotation(pvList.moves[i]) << " ";
