95e00ada9b2727a0cf95f79a8fbdfaaa2bc0ffd1
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2025-12-05 13:39:28 +0100

    Update params after tuning

--- src/params.h
+++ src/params.h
@@ -37,20 +37,20 @@ namespace zaphod::params {
         }
 
 	// LMR
-	ZAP_TUNABLE_INT(lmrDividerQuiet, 225, 100, 350, 10)
-	ZAP_TUNABLE_INT(lmrDividerNoisy, 229, 100, 350, 10)
-	ZAP_TUNABLE_INT(lmrBaseQuiet, 83, 0, 150, 10)
+	ZAP_TUNABLE_INT(lmrDividerQuiet, 221, 100, 350, 10)
+	ZAP_TUNABLE_INT(lmrDividerNoisy, 226, 100, 350, 10)
+	ZAP_TUNABLE_INT(lmrBaseQuiet, 87, 0, 150, 10)
 	ZAP_TUNABLE_INT(lmrBaseNoisy, -7, -100, 100, 10)
-	ZAP_TUNABLE_INT(lmrPVReduction, 102, 0, 200, 10)
-	ZAP_TUNABLE_INT(lmrImprovingReduction, 100, 0, 200, 10)
-	ZAP_TUNABLE_INT(lmrCheckReduction, 98, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrPVReduction, 101, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrImprovingReduction, 99, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrCheckReduction, 101, 0, 200, 10)
 
 	//Razoring
-	ZAP_TUNABLE_INT(razoringMargin, 203, 100, 400, 20)
+	ZAP_TUNABLE_INT(razoringMargin, 210, 100, 400, 20)
 
 	//Qsearch
-	ZAP_TUNABLE_INT(futilityBaseQsearch, 200, 0, 300, 20)
-	ZAP_TUNABLE_INT(seeMarginQsearch, -150, -200, 100, 20)
+	ZAP_TUNABLE_INT(futilityBaseQsearch, 195, 0, 300, 20)
+	ZAP_TUNABLE_INT(seeMarginQsearch, -153, -200, 100, 20)
 
 };
 #endif
