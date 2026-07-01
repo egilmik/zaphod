beae3a021d2bf5d6237bc093b4c251892ac8e690
Egil Tennfjord Mikalsen <egilmikalsen@gmail.com>
2026-07-01 18:18:24 +0200

    Disabled razoring and null move in negamax

--- src/search.cpp
+++ src/search.cpp
@@ -250,6 +250,7 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
     ////////////
     // Razoring
     ////////////
+    /*
     if (!isRoot && depth <= 3 && (ss[ply].staticEval + razoringMargin() * depth) < beta) {
 
         int value = quinesence(board, alpha, beta, 0, ply, false);
@@ -258,6 +259,7 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
         }
     }
     
+    */
     
     ////////////
     // Reverse futility pruning
@@ -282,6 +284,7 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
     ////////////
     // Null move pruning
     ////////////
+    /*
     if (!pvNode && !inCheck  && ss[ply].staticEval >= beta && depth >= 3 && !isRoot && !ss[ply - 1].isNullMove) {
         if(board.getNonPawnMaterial(board.getSideToMove()) > 0 ){
             int R = 3 + (depth >= 6) + improving;
@@ -296,7 +299,7 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
             }
         }
     }
-
+    */
     
     int moveCounter = 0;
     Move move;
