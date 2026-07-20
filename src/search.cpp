cf866488d334e97be3326c952e12a94bc474a88c
Egil Mikalsen <egilmikalsen@gmail.com>
2026-07-20 10:47:59 +0200

    Reintroduced depth+1 for linear term in rfp

--- src/search.cpp
+++ src/search.cpp
@@ -269,7 +269,7 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
     ////////////
     // Reverse futility pruning
     ////////////
-    int futilityMargin = depth * rfpLinear();
+    int futilityMargin =(1+ depth) * rfpLinear();
     futilityMargin += rfpQuadratic()*depth*depth;
     //futilityMargin -= rfpImproving()*improving;
     if (!pvNode && !inCheck && depth <= 6 && (ss[ply].staticEval - futilityMargin >= beta) && ss[ply].staticEval < MATESCORE-MAXPLY) {
