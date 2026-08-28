811fb14f4466b78ae1c47f91a287d8ddf29e4ac9
Egil Mikalsen <egilmikalsen@gmail.com>
2026-08-28 08:38:55 +0200

    Updated tunables from SPSA run

--- src/params.h
+++ src/params.h
@@ -37,42 +37,42 @@ namespace zaphod::params {
         }
 
 	// LMR
-	ZAP_TUNABLE_INT(lmrDividerQuiet, 184, 100, 350, 10)
-	ZAP_TUNABLE_INT(lmrDividerNoisy, 221, 100, 350, 10)
-	ZAP_TUNABLE_INT(lmrBaseQuiet, 96, 0, 150, 10)
+	ZAP_TUNABLE_INT(lmrDividerQuiet, 188, 100, 350, 10)
+	ZAP_TUNABLE_INT(lmrDividerNoisy, 217, 100, 350, 10)
+	ZAP_TUNABLE_INT(lmrBaseQuiet, 103, 0, 150, 10)
 	ZAP_TUNABLE_INT(lmrBaseNoisy, -41, -100, 100, 10)
-	ZAP_TUNABLE_INT(lmrPVReduction, 79, 0, 200, 10)
-	ZAP_TUNABLE_INT(lmrImprovingReduction, 87, 0, 200, 10)
-	ZAP_TUNABLE_INT(lmrCheckReduction, 104, 0, 200, 10)
-	ZAP_TUNABLE_INT(lmrHistoryReduction, 250,0,600,20)
+	ZAP_TUNABLE_INT(lmrPVReduction, 85, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrImprovingReduction, 85, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrCheckReduction, 92, 0, 200, 10)
+	ZAP_TUNABLE_INT(lmrHistoryReduction, 222,0,600,20)
 
 	//Razoring
-	ZAP_TUNABLE_INT(razoringMargin, 271, 100, 400, 20)
+	ZAP_TUNABLE_INT(razoringMargin, 263, 100, 400, 20)
 
 	//Qsearch
-	ZAP_TUNABLE_INT(futilityBaseQsearch, 164, 0, 300, 20)
+	ZAP_TUNABLE_INT(futilityBaseQsearch, 153, 0, 300, 20)
 //	ZAP_TUNABLE_INT(seeMarginQsearch, -166, -200, 100, 20)
 
 	//Reverse Futility Pruning
-	ZAP_TUNABLE_INT(rfpLinear, 97, 0, 200, 20)
+	ZAP_TUNABLE_INT(rfpLinear, 41, 0, 200, 20)
 //	ZAP_TUNABLE_INT(rfpQuadratic, 10, 0, 200, 20)
-	ZAP_TUNABLE_INT(rfpImproving, 104, 0, 200, 20)
+	ZAP_TUNABLE_INT(rfpImproving, 93, 0, 200, 20)
 
 	//History
-	ZAP_TUNABLE_INT(quietHistBonusDepthScale, 325, 0, 500, 20)
-	ZAP_TUNABLE_INT(quietHistBonusOffset, 333, 0, 500, 20)
-	ZAP_TUNABLE_INT(quietHistMaxBonus, 16129, 0, 32000, 256)
-	ZAP_TUNABLE_INT(quietHistPenaltyDepthScale, 300, 0, 500, 20)
-	ZAP_TUNABLE_INT(quietHistPenaltyOffset, 292, 0, 500, 20)
-	ZAP_TUNABLE_INT(quietHistMaxPenalty, 15835, 0, 32000, 256)
-	ZAP_TUNABLE_INT(maxButterflyHistory, 16240, 0, 32000, 256)
-	ZAP_TUNABLE_INT(butterflyAging, 750, 0, 2000,50) 
-	ZAP_TUNABLE_INT(contAging, 750, 0, 2000,50) 
+	ZAP_TUNABLE_INT(quietHistBonusDepthScale, 318, 0, 500, 20)
+	ZAP_TUNABLE_INT(quietHistBonusOffset, 312, 0, 500, 20)
+	ZAP_TUNABLE_INT(quietHistMaxBonus, 16150, 0, 32000, 256)
+	ZAP_TUNABLE_INT(quietHistPenaltyDepthScale, 282, 0, 500, 20)
+	ZAP_TUNABLE_INT(quietHistPenaltyOffset, 260, 0, 500, 20)
+	ZAP_TUNABLE_INT(quietHistMaxPenalty, 15942, 0, 32000, 256)
+	ZAP_TUNABLE_INT(maxButterflyHistory, 16042, 0, 32000, 256)
+	ZAP_TUNABLE_INT(butterflyAging, 787, 0, 2000,50) 
+	ZAP_TUNABLE_INT(contAging, 727, 0, 2000,50) 
 
-	ZAP_TUNABLE_INT(maxContHistory, 16000, 256, 32000, 256)
-	ZAP_TUNABLE_INT(contWeight1Ply, 120, 0, 200, 10)
-	ZAP_TUNABLE_INT(contWeight2Ply, 120, 0, 200, 10)
-	ZAP_TUNABLE_INT(contWeight4Ply, 60, 0, 200, 10)
-	ZAP_TUNABLE_INT(contWeight6Ply, 30, 0, 200, 10)
+	ZAP_TUNABLE_INT(maxContHistory, 16087, 256, 32000, 256)
+	ZAP_TUNABLE_INT(contWeight1Ply, 110, 0, 200, 10)
+	ZAP_TUNABLE_INT(contWeight2Ply, 113, 0, 200, 10)
+	ZAP_TUNABLE_INT(contWeight4Ply, 77, 0, 200, 10)
+	ZAP_TUNABLE_INT(contWeight6Ply, 38, 0, 200, 10)
 };
 #endif
