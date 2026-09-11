41437e2a87ac116b00b321f15f2822175c3a506d
Egil Mikalsen <egilmikalsen@gmail.com>
2026-09-11 12:38:49 +0200

    Revert "Changed scaling of correction history lmr term"
    This reverts commit 5ec21394c94ac4856d4518e24fa6833374e84ca7.

--- spsa.sh
+++
@@ -1,4 +0,0 @@
-cmake --build --preset linux-gcc
-python3 scripts/gen_tune_config.py src/params.h -o config.json
-mv config.json ../weather-factory/
-mv build/linux-gcc/bin/Zaphod ../weather-factory/tuner/
