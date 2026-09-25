ed3c50c8ecd2fabc7032a6d1088ac9bbb941aea0
Egil Mikalsen <egilmikalsen@gmail.com>
2026-09-25 15:04:17 +0200

    Added internal iterative reduction

--- src/search.cpp
+++ src/search.cpp
@@ -281,6 +281,15 @@ int Search::negamax(Board& board, int depth, int alpha, int beta, int ply, bool 
     }
 
     ////////////
+    // Internal Iterative Reduction
+    ///////////
+    
+    if(depth > 6 && !inCheck && !ttHit){
+        depth--;
+    }
+
+
+    ////////////
     // Razoring
     ////////////
     if (!isRoot && !ttHit && depth <= 4 && ss[ply].staticEval < (alpha - razoringMargin() *depth) ) {
